089ad1183fa948a06e27ad1eafd819b3eb2728ea
mezosalama20006-boop <mezosalama20006@gmail.com>
2026-06-24 05:22:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 BOT_TOKEN = os.getenv("TOKEN")
 DATABASE_PATH = os.getenv("DATABASE_PATH", "store.db")
+if not os.path.isabs(DATABASE_PATH):
+    DATABASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), DATABASE_PATH))
 
 ADMIN_IDS = [6098463726]
 EGP_EXCHANGE_RATE = 55
